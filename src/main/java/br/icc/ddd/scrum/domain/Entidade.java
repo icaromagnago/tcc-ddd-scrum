aa8c7dd833d0db7feb2fd2b491197fe20f3e4918
Ícaro Geraldo Magnago <icaro.magnago-ext@ANBIMA.COM.INT>
2016-03-11 17:42:55 -0300

    Classe Abstrata Entidade

--- src/main/java/br/icc/ddd/scrum/domain/Entidade.java
+++ src/main/java/br/icc/ddd/scrum/domain/Entidade.java
@@ -1,5 +1,31 @@
 package br.icc.ddd.scrum.domain;
 
-public abstract class Entidade {
+import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+/**
+ * Toda entidade do sistema deve herdar desta classe abstrata
+ */
+@MappedSuperclass
+public abstract class Entidade implements Serializable {
+
+	private static final long serialVersionUID = 4394209939156982282L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id", unique = true)
+	protected Long id;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
 }
